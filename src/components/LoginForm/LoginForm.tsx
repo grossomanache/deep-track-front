b5fb4d05c354c9d27220f4b15bc48ba0c3d2bc28
Elías Heresi <elias.heresi@gmail.com>
2022-07-15 09:38:30 -0400

    User input in login form modifies component state

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,5 +1,5 @@
 import { Box, Button, TextField, Typography } from "@mui/material";
-import { FunctionComponent, useState } from "react";
+import { ChangeEvent, FunctionComponent, useState, useEffect } from "react";
 import { LoginUser } from "../../redux/interfaces/UserInterfaces";
 
 const LoginForm: FunctionComponent = (): JSX.Element => {
@@ -8,11 +8,28 @@ const LoginForm: FunctionComponent = (): JSX.Element => {
   const [formData, setFormData] = useState(formInitialState);
   const [buttonDisabled, setButtonDisabled] = useState(true);
 
+  const changeData = (event: ChangeEvent<HTMLInputElement>): void => {
+    setFormData({ ...formData, [event.target.id]: event.target.value });
+  };
+
+  const resetForm = (): void => {
+    setFormData(formInitialState);
+  };
+
+  useEffect(() => {
+    if (formData.password && formData.username) {
+      setButtonDisabled(false);
+      return;
+    }
+    setButtonDisabled(true);
+  }, [formData]);
+
   return (
     <>
       <Box
         component="form"
         noValidate
+        autoComplete="off"
         sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
       >
         <Typography component="h1" variant="h5">
@@ -20,11 +37,12 @@ const LoginForm: FunctionComponent = (): JSX.Element => {
         </Typography>
         <TextField
           required
-          id="username-input"
           name="username"
           label="Username"
           type="text"
-          autoComplete="off"
+          id="username"
+          value={formData.username}
+          onChange={changeData}
         />
         <TextField
           required
@@ -32,9 +50,10 @@ const LoginForm: FunctionComponent = (): JSX.Element => {
           label="Password"
           type="password"
           id="password"
-          autoComplete="current-password"
+          value={formData.password}
+          onChange={changeData}
         />
-        <Button type="submit" variant="contained">
+        <Button type="submit" variant="contained" disabled={buttonDisabled}>
           Login{" "}
         </Button>
       </Box>
